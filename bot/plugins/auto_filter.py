d537e2686ff5fd6a9fa227d758b236119539fec1
Georgiprajan <85634616+Georgiprajan@users.noreply.github.com>
2021-06-30 15:48:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}📁{file_name}"
+            button_text = f"{file_size}{file_name}"
             
 
             if file_type == "video":
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Check Links At @requestmoviesmalayalam Found {(len_results)} Results For Your Request 😺: <code>{query}</code>",
+                text=f"Join New Release Where The links given other wise you can't access The Content Or Click This Link For New HD Release https://t.me/joinchat/btN1KIZzIu1kMmM1 or This Link For Joining And Accessing New Movie Links https://t.me/joinchat/0gx-M_5sCoMyZGFlCheck Links At @requestmoviesmalayalam Found {(len_results)} Results For Your Request 😺: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
